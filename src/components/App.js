7011614c4d13aff599639360085a7d5257e48b02
M. Sertaç Toröz <msertactoroz@Ms-MacBook-Air.local>
2022-12-01 12:56:30 +0300

    components added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
-import logo from "./logo.svg";
+import logo from "../logo.svg";
 
 
 function App() {
   return <div className="App">
+  <h3>{process.env.NODE_ENV}</h3>
  <img src="/logo192.png" alt=""></img>
  <img src={logo} alt=""></img>
   </div>;
